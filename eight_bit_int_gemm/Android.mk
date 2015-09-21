d78999fcc21cc1c7b46c51cfe1bf2572f6f3a579
Miao Wang <miaowang@google.com>
2015-09-21 16:54:10 -0700

    making the gemmlowp token more descriptive
    Change-Id: Ieb8a9484b9c207afd70618a11b5abe22d661b438

--- eight_bit_int_gemm/Android.mk
+++ eight_bit_int_gemm/Android.mk
@@ -36,7 +36,7 @@ LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES:= eight_bit_int_gemm.cc
 
 LOCAL_CFLAGS += -no-integrated-as -std=c++11
-LOCAL_CFLAGS += -DGEMMLOWP_USE_OLD_ANDROID_SDK
+LOCAL_CFLAGS += -DGEMMLOWP_USE_STLPORT
 LOCAL_C_INCLUDES += external/gemmlowp/
 LOCAL_NDK_STL_VARIANT := stlport_static
 
