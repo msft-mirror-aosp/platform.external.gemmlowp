9720260bffb42c19bc49cea73a9d766851276fed
Miao Wang <miaowang@google.com>
2015-09-22 13:46:48 -0700

    Always build gemmlowpV8 for support lib using NEON
    Change-Id: Ied4bd4670e0e980e6bfcce71852338aa5d4a7d19

--- eight_bit_int_gemm/Android.mk
+++ eight_bit_int_gemm/Android.mk
@@ -31,10 +31,14 @@ LOCAL_CLANG := true
 LOCAL_MODULE := libbnnmlowpV8
 
 LOCAL_SDK_VERSION := 8
-
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES:= eight_bit_int_gemm.cc
 
+# Always build gemmlowpV8 for armv7 using NEON,
+# alternate non-SIMD route is built in libRSSupport.
+ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
+LOCAL_CFLAGS_arm := -mfpu=neon
+endif
 LOCAL_CFLAGS += -no-integrated-as -std=c++11
 LOCAL_CFLAGS += -DGEMMLOWP_USE_STLPORT
 LOCAL_C_INCLUDES += external/gemmlowp/
